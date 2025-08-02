d2c7f5c20a71ea0c1d3cec95a70a16dacdc728c3
Javainux <mateinge47@gmail.com>
2025-08-02 01:02:14 -0600

    Se creo el paquete Exception, asi como el archivo GlobalException.java

---
+++ src/main/java/com/MiDoc/Midoc/Exception/GlobalException.java
@@ -0,0 +1,24 @@
+package com.MiDoc.Midoc.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+            errores.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errores);
+    }
+}
