339222d9f12710a10cd309a7249c7ea88a362a97
Javainux <mateinge47@gmail.com>
2025-08-04 00:44:01 -0600

    Terminando la clase principal

--- src/main/java/com/MiDoc/Midoc/MidocApplication.java
+++ src/main/java/com/MiDoc/Midoc/MidocApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MidocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MidocApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MidocApplication.class, args);
+    }
 }
