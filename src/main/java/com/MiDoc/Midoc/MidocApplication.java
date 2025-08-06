49a0b75fffa5a1ddc3d3a669eaddc358c2b6cdf7
Javainux <mateinge47@gmail.com>
2025-08-06 07:14:05 -0600

    1

--- src/main/java/com/MiDoc/Midoc/MidocApplication.java
+++ src/main/java/com/MiDoc/Midoc/MidocApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MidocApplication {
 
     public static void main(String[] args) {
+        String port = System.getenv("PORT");
+        if (port != null) {
+            System.setProperty("server.port", port);
+        }
         SpringApplication.run(MidocApplication.class, args);
-        System.out.println("✅ MidocApplication arrancó correctamente."); 
     }
 }
