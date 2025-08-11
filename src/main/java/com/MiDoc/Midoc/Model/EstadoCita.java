e87a893980e1f469273298a160712ad9ac956c9d
Javainux <mateinge47@gmail.com>
2025-08-11 08:31:46 -0600

    Permitir mapeo flexible de EstadoCita desde frontend (case-insensitive)

--- src/main/java/com/MiDoc/Midoc/Model/EstadoCita.java
+++ src/main/java/com/MiDoc/Midoc/Model/EstadoCita.java
@@ -1,8 +1,19 @@
 package com.MiDoc.Midoc.Model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum EstadoCita {
     PENDIENTE,
     CONFIRMADA,
     CANCELADA,
-    COMPLETADA
+    COMPLETADA;
+
+    @JsonCreator
+    public static EstadoCita fromString(String value) {
+        try {
+            return EstadoCita.valueOf(value.toUpperCase());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("EstadoCita inválido: " + value);
+        }
+    }
 }
